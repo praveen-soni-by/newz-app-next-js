935e5f867ba21cbdb9cbb0227f4f26b75c2550f8
praveen-soni-by <praveen.soni@blueyonder.com>
2021-09-25 12:11:26 +0530

    initial setup - next,react,tailwind,eslint'

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <div>
+      <header>
+         Hell
+      </header>
+
+      <article>Body</article>
+      <footer>Footer</footer>
+    </div>
+  );
+}
