7186b17ba32f32bf99eb023b59a07d247591db3f
praveen-soni-by <praveen.soni@blueyonder.com>
2021-11-01 18:47:13 +0530

    added scroll to up button

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,13 @@ import React, { useState, useCallback } from "react";
 import _debounce from "lodash/debounce";
 import Router from "next/router";
 import { useRouter } from 'next/router';
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons"; // import the icons you need
 
 import Navbar from "../components/Navbar"
 import Loader from "../components/Loader";
 import NewsService from "../service/NewsService";
 import { withHeadTitle } from "../components/hoc/withHeadTitle";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
@@ -20,6 +22,8 @@ function MyApp({ Component, pageProps }) {
   const [isLoading, setLoading] = useState(false);
   const debounceFn = useCallback(_debounce(handleDebounceFn, 500), []);
   const router = useRouter();
+
+  const srcollRef = React.useRef();
 
   async function handleDebounceFn(inputValue) {
     setLoading(true);
@@ -64,10 +68,28 @@ function MyApp({ Component, pageProps }) {
   React.useEffect(() => setValue(""), [router.asPath])
 
   return (
-    <div className="mx-auto overflow-x-hidden">
+    <div className="mx-auto overflow-x-hidden" ref={srcollRef}>
       <Navbar search={handleChange} value={value} clearSearch={() => setValue("")} />
+
       {isLoading ? <Loader />
         : <Component {...pageProps} isLoading={isLoading} searchNews={searchNews} isSearch={value} />}
+
+      <div className="flex justify-end content-center ">
+        <button className="bg-blue-500
+          focus-within:  mb-10  mr-5
+        hover:bg-blue-500 text-white font-bold py-2 px-2 "
+          onClick={() => {
+            window.scrollTo({
+              top: 0,
+              behavior: "smooth"
+            })
+          }} >
+
+          <FontAwesomeIcon
+            color="primary"
+            icon={faArrowUp}
+          ></FontAwesomeIcon>
+        </button> </div>
     </div>
   )
 }
